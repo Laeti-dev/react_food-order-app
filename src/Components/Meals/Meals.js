b0f36c5a06e29508d7b1f9f0e18c57dd8b94fee4
Laeti-dev <laetdi@gmail.com>
2023-09-16 17:05:43 +0200

    mealList from DUMMY object

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-import Card from "../../UI/Card/Card";
-import MealItem from "./MealItem";
+import MealsList from "./MealsList";
 import MealsSummary from "./MealsSummary";
-
-import styles from "./Meals.module.css";
 
 const Meals = () => {
   return(
     <React.Fragment>
         <MealsSummary />
-        {/* map through meals items object */}
-        <MealItem />
+        <MealsList />
     </React.Fragment>
   )
 };
