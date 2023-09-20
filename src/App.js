ddde06b2f26a20a82d98acb95f22a994d7e18d60
Laeti-dev <laetdi@gmail.com>
2023-09-20 05:06:31 +0200

    modal styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './Components/Header/Header';
 import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
+      <Cart />
       <Header />
       <Meals />
     </React.Fragment>
