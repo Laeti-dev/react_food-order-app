4586b69481949b483d9dc1f108e224fe472685fa
Laeti-dev <laetdi@gmail.com>
2023-09-26 05:51:41 +0200

    reviewing context logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,47 +2,26 @@ import React, {useReducer} from "react";
 
 import CartContext from "./cart-context";
 
-const cartState = {
+const defaultCartState = {
   items: [],
   total: 0
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === "ADD_ITEM"){
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = state.items.concat(action.item);
     const updatedTotal = state.total + action.item.price * action.item.quantity;
-
-    // finds index if item exists
-    const existingItemIndex = state.items.findIndex(item => item.id === action.item.id);
-    // get the item by its index
-    const existingItem = state.items[existingItemIndex];
-    let updatedItems;
-
-    // if item exists in items
-    if (existingItem) {
-      // create a new object taking all the former object items
-      // and calculate qty
-      const updatedItem = {
-        ...existingItem,
-        total: existingItem.total + action.item.total
-      };
-      // update items array
-      updatedItems= [...state.items ];
-      updatedItems[existingItemIndex] = updatedItem
-    } else {
-      updatedItems = state.items.concat(action.item)
-    }
-    // const updatedItems = state.items.concat(action.item);
 
     return {
       items: updatedItems,
-      total: updatedTotal
+      total: updatedTotal,
     };
   }
-  return cartState;
+  return defaultCartState;
 };
 
 const CartProvider = (props) => {
-  const [cartStateUpdate, dispatchCartAction] = useReducer(cartReducer, cartState);
+  const [cartStateUpdate, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
   const addToCartHandler = (item) => {
     dispatchCartAction({type: "ADD_ITEM", item:item});
