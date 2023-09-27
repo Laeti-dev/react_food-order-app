be4dfc74d5cc16343d927b1ea092e08dbb0626c9
Laeti-dev <laetdi@gmail.com>
2023-09-27 21:03:01 +0200

    remove item in cartprovider logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -14,20 +14,43 @@ const cartReducer = (state, action) => {
       // if true, returns item's index
       (item) => item.id === action.item.id
     );
+    // find the item with its index
     const existingItem = state.items[cartItemIndex];
     let updatedItems;
 
-    if (existingItem) {
-      const updatedItem = {
-        ...existingItem,
-        quantity: existingItem.quantity + action.item.quantity
+    if (existingItem) { // if this item exists in cart array
+      const updatedItem = { // define a new object
+        ...existingItem, //taking all the values for this item
+        quantity: existingItem.quantity + action.item.quantity // adding the new qantity
       };
+
       updatedItems = [...state.items];
       updatedItems[cartItemIndex] = updatedItem
+
     } else {
       updatedItems = state.items.concat(action.item);
     }
 
+    return {
+      items: updatedItems,
+      total: updatedTotal,
+    };
+  }
+  if (action.type === "REMOVE_ITEM"){
+    const existingItemIndex = state.items.findIndex(
+      // if true, returns item's index
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingItemIndex];
+    const updatedTotal = state.total - existingItem.price;
+    let updatedItems;
+    if(existingItem.quantity === 1) {
+      updatedItems.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = {...existingItem, amount: existingItem.amount -1};
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    };
     return {
       items: updatedItems,
       total: updatedTotal,
